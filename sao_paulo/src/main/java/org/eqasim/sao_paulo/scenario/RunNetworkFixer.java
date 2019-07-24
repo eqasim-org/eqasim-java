aecb2d291c93b9c19ea1689f8e2009e013c4fc17
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-07-25 01:41:30 +0200

    Add Sao Paulo (#2)

---
+++ sao_paulo/src/main/java/org/eqasim/sao_paulo/scenario/RunNetworkFixer.java
@@ -0,0 +1,24 @@
+package org.eqasim.sao_paulo.scenario;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.network.io.NetworkWriter;
+
+public class RunNetworkFixer {
+	static public void main(String[] args) throws ConfigurationException {
+		CommandLine cmd = new CommandLine.Builder(args) //
+				.requireOptions("input-path", "output-path") //
+				.build();
+
+		Network network = NetworkUtils.createNetwork();
+		new MatsimNetworkReader(network).readFile(cmd.getOptionStrict("input-path"));
+
+		NetworkFixer networkFixer = new NetworkFixer();
+		networkFixer.run(network);
+
+		new NetworkWriter(network).write(cmd.getOptionStrict("output-path"));
+	}
+}
