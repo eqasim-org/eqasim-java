e5e7be161dcc1c03b013ef107107490f95d6d551
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-08-17 18:03:35 +0200

    Add OVGK support (#5)

---
+++ switzerland/src/main/java/org/eqasim/switzerland/ovgk/OVGKConstants.java
@@ -0,0 +1,11 @@
+package org.eqasim.switzerland.ovgk;
+
+public final class OVGKConstants {
+	private OVGKConstants() {
+	}
+
+	static public final String STOP_CATEGORY_ATTRIBUTE = "stopCategory";
+
+	static public final int WORST_STOP_CATEGORY = 5;
+	static public final OVGK BEST_OVGK = OVGK.A;
+}
