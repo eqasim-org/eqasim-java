cb805d9d34c3add8d45da20c7f085f1a708c5b0e
Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
2019-09-17 00:49:35 +0200

    Add automated_vehicles and examples packages (#11)
    * Start automated vehicles
    
    * WIP
    
    * Add autoamted_vehicles and examples packages

---
+++ automated_vehicles/src/main/java/org/eqasim/automated_vehicles/mode_choice/constraints/AvWalkConstraint.java
@@ -0,0 +1,50 @@
+package org.eqasim.automated_vehicles.mode_choice.constraints;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+
+import ch.ethz.matsim.discrete_mode_choice.model.DiscreteModeChoiceTrip;
+import ch.ethz.matsim.discrete_mode_choice.model.constraints.AbstractTripConstraint;
+import ch.ethz.matsim.discrete_mode_choice.model.trip_based.TripConstraint;
+import ch.ethz.matsim.discrete_mode_choice.model.trip_based.TripConstraintFactory;
+import ch.ethz.matsim.discrete_mode_choice.model.trip_based.candidates.RoutedTripCandidate;
+import ch.ethz.matsim.discrete_mode_choice.model.trip_based.candidates.TripCandidate;
+
+public class AvWalkConstraint extends AbstractTripConstraint {
+	@Override
+	public boolean validateAfterEstimation(DiscreteModeChoiceTrip trip, TripCandidate candidate,
+			List<TripCandidate> previousCandidates) {
+		if (candidate.getMode().equals("av")) {
+			if (candidate instanceof RoutedTripCandidate) {
+				// Go through all plan elments
+				for (PlanElement element : ((RoutedTripCandidate) candidate).getRoutedPlanElements()) {
+					if (element instanceof Leg) {
+						if (((Leg) element).getMode().equals("av")) {
+							// If we find at least one av leg, we're good
+							return true;
+						}
+					}
+				}
+
+				// If there was no av leg, we do not accept this candidate
+				return false;
+			} else {
+				throw new IllegalStateException("Need a route to evaluate constraint");
+			}
+		}
+
+		return true;
+	}
+
+	static public class Factory implements TripConstraintFactory {
+		@Override
+		public TripConstraint createConstraint(Person person, List<DiscreteModeChoiceTrip> trips,
+				Collection<String> availableModes) {
+			return new AvWalkConstraint();
+		}
+	}
+}
---
+++ examples/src/main/java/org/eqasim/examples/zurich_av/RunSimulation.java
@@ -0,0 +1,76 @@
+package org.eqasim.examples.zurich_av;
+
+import org.eqasim.automated_vehicles.mode_choice.AvConfigurator;
+import org.eqasim.core.components.transit.EqasimTransitQSimModule;
+import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
+import org.eqasim.switzerland.SwitzerlandConfigurator;
+import org.eqasim.switzerland.mode_choice.SwissModeChoiceModule;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import ch.ethz.matsim.av.framework.AVQSimModule;
+import ch.ethz.matsim.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
+
+/**
+ * This is an example run script that runs the Switzerland/Zurich scenario with
+ * automated vehicles. This script basically resembles the basic RunSimulation
+ * script from the Switzerland scenario, but with a couple of added components
+ * to use the AV module. They are marked further below.
+ */
+public class RunSimulation {
+	static public void main(String[] args) throws ConfigurationException {
+		CommandLine cmd = new CommandLine.Builder(args) //
+				.requireOptions("config-path") //
+				.build();
+
+		Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"),
+				SwitzerlandConfigurator.getConfigGroups());
+		AvConfigurator.configure(config); // Add some configuration for AV
+		cmd.applyConfiguration(config);
+
+		// While the tools from the AV module conveniently set everything up to use the
+		// mode, we still need to give the option to agents through the ModeAvailability
+		// in mode choice. Our custom ModeAvailabilty for this Zurich/AV example is
+		// configured here. The implmenetaton will be added further below when setting
+		// up the controller and the modules.
+		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules()
+				.get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
+		dmcConfig.setModeAvailability(ZurichAvModule.ZURICH_AV_MODE_AVAILABILITY_NAME);
+
+		Scenario scenario = ScenarioUtils.createScenario(config);
+
+		SwitzerlandConfigurator.configureScenario(scenario);
+		ScenarioUtils.loadScenario(scenario);
+		SwitzerlandConfigurator.adjustScenario(scenario);
+
+		// The AvConfigurator provides some convenience functions to adjust the
+		// scenario. Here, we add the mode 'av' to all links that have the 'car' mode
+		// and define that all links belong to one waiting time estimation group (i.e.
+		// we estimate an overall waiting time average over all links).
+		AvConfigurator.configureCarLinks(scenario);
+		AvConfigurator.configureUniformWaitingTimeGroup(scenario);
+
+		Controler controller = new Controler(scenario);
+		SwitzerlandConfigurator.configureController(controller);
+		controller.addOverridingModule(new EqasimModeChoiceModule());
+		controller.addOverridingModule(new SwissModeChoiceModule(cmd));
+		AvConfigurator.configureController(controller, cmd); // Add some modules for AV
+
+		// Here we add our custom Zurich/AV module to add our specific ModeAvailability
+		controller.addOverridingModule(new ZurichAvModule());
+
+		// This is not totally obvious, but we need to adjust the QSim components if we
+		// have AVs
+		controller.configureQSimComponents(configurator -> {
+			EqasimTransitQSimModule.configure(configurator);
+			AVQSimModule.configureComponents(configurator);
+		});
+
+		controller.run();
+	}
+}
