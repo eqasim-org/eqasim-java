97f7a8e54ba8017697632bc14fb516e13743f5d0
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-09-19 18:26:24 +0200

    Improve trip analysis (#18)
    * Improve analysis
    
    * Add trip analysis to AV
    
    * Update CHANGELOG.md

---
+++ core/src/main/java/org/eqasim/core/analysis/DefaultPersonAnalysisFilter.java
@@ -0,0 +1,11 @@
+package org.eqasim.core.analysis;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+
+public class DefaultPersonAnalysisFilter implements PersonAnalysisFilter {
+	@Override
+	public boolean analyzePerson(Id<Person> personId) {
+		return true;
+	}
+}
