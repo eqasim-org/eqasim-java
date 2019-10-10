26dbb37b38e1712f6316f2e3ca30ebb47f394f4d
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-10-10 21:22:49 +0200

    Auckland (#28)
    * Auckland WIP
    
    * WIP
    
    * Move example to other repositroy

---
+++ auckland/src/main/java/org/eqasim/auckland/mode_choice/AucklandModeAvailability.java
@@ -0,0 +1,20 @@
+package org.eqasim.auckland.mode_choice;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Person;
+
+import ch.ethz.matsim.discrete_mode_choice.model.DiscreteModeChoiceTrip;
+import ch.ethz.matsim.discrete_mode_choice.model.mode_availability.ModeAvailability;
+
+public class AucklandModeAvailability implements ModeAvailability {
+	static public final String NAME = "AucklandModeAvailability";
+
+	@Override
+	public Collection<String> getAvailableModes(Person person, List<DiscreteModeChoiceTrip> trips) {
+		return Arrays.asList(TransportMode.car, TransportMode.pt, TransportMode.walk);
+	}
+}
