bc2c42ae9fb0269a1c404fae0c1ef93fb56b96a3
balacm <milos.balac@ivt.baug.ethz.ch>
2019-11-26 09:11:17 +0100

    update pt costs

--- los_angeles/src/main/java/org/eqasim/los_angeles/mode_choice/parameters/LosAngelesCostParameters.java
+++ los_angeles/src/main/java/org/eqasim/los_angeles/mode_choice/parameters/LosAngelesCostParameters.java
@@ -13,8 +13,8 @@ public class LosAngelesCostParameters implements ParameterDefinition {
 		LosAngelesCostParameters parameters = new LosAngelesCostParameters();
 
 		parameters.carCost_USD_mile = 0.2;
-		parameters.ptCostPerTrip_USD_20km = 2.5;
-		parameters.ptCostPerTrip_USD_40km = 2.0;
+		parameters.ptCostPerTrip_USD_20km = 1.75;
+		parameters.ptCostPerTrip_USD_40km = 1.75;
 		parameters.ptCostPerTrip_USD_40plus_km = 1.5;
 
 		return parameters;
