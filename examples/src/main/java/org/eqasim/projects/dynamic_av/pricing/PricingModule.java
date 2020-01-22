44b9db44dc2325cc465a353da667ff825f1f8a46
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-01-22 13:57:31 +0100

    Add plot for prices

--- examples/src/main/java/org/eqasim/projects/dynamic_av/pricing/PricingModule.java
+++ examples/src/main/java/org/eqasim/projects/dynamic_av/pricing/PricingModule.java
@@ -8,9 +8,10 @@ import org.eqasim.core.simulation.mode_choice.AbstractEqasimExtension;
 import org.eqasim.core.simulation.mode_choice.ParameterDefinition;
 import org.eqasim.projects.dynamic_av.pricing.cost_calculator.CostCalculator;
 import org.eqasim.projects.dynamic_av.pricing.cost_calculator.definitions.ScenarioDefinition;
+import org.eqasim.projects.dynamic_av.pricing.price.FinancialInformationPlotter;
+import org.eqasim.projects.dynamic_av.pricing.price.FinancialInformationWriter;
 import org.eqasim.projects.dynamic_av.pricing.price.PriceCalculator;
 import org.eqasim.projects.dynamic_av.pricing.price.ProjectAvCostModel;
-import org.eqasim.projects.dynamic_av.pricing.price.FinancialInformationWriter;
 import org.eqasim.projects.dynamic_av.pricing.price.ProjectCostParameters;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
@@ -40,6 +41,7 @@ public class PricingModule extends AbstractEqasimExtension {
 		bindCostModel(PROJECT_AV_COST_MODEL_NAME).to(ProjectAvCostModel.class);
 
 		addControlerListenerBinding().to(FinancialInformationWriter.class);
+		addControlerListenerBinding().to(FinancialInformationPlotter.class);
 	}
 
 	@Provides
@@ -67,10 +69,17 @@ public class PricingModule extends AbstractEqasimExtension {
 
 	@Provides
 	@Singleton
-	public FinancialInformationWriter provideProjectAvCostWriter(OutputDirectoryHierarchy outputDirectory,
+	public FinancialInformationWriter provideFinancialInformationWriter(OutputDirectoryHierarchy outputDirectory,
 			PriceCalculator calculator) {
 		File outputPath = new File(outputDirectory.getOutputFilename("project_av_prices.csv"));
 		return new FinancialInformationWriter(outputPath, calculator);
+	}
+
+	@Provides
+	@Singleton
+	public FinancialInformationPlotter provideFinancialInformationPlotter(OutputDirectoryHierarchy outputDirectory,
+			PriceCalculator calculator) {
+		return new FinancialInformationPlotter(calculator, outputDirectory);
 	}
 
 	@Provides
