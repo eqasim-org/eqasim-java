d42e1c21049d4aca261716f3ab17c47661ea837d
AuroreSallard <Aurore.Sallard@ivt.baug.ethz.ch>
2020-02-03 17:34:36 +0100

    Bounded TaxiPredictor in ModeChoiceModule

--- sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/SaoPauloModeChoiceModule.java
+++ sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/SaoPauloModeChoiceModule.java
@@ -9,6 +9,7 @@ import org.eqasim.core.simulation.mode_choice.AbstractEqasimExtension;
 import org.eqasim.core.simulation.mode_choice.ParameterDefinition;
 import org.eqasim.core.simulation.mode_choice.cost.CostModel;
 import org.eqasim.core.simulation.mode_choice.parameters.ModeParameters;
+import org.eqasim.core.simulation.mode_choice.utilities.predictors.CarPredictor;
 import org.eqasim.sao_paulo.mode_choice.costs.SaoPauloCarCostModel;
 import org.eqasim.sao_paulo.mode_choice.costs.SaoPauloPtCostModel;
 import org.eqasim.sao_paulo.mode_choice.costs.SaoPauloTaxiCostModel;
@@ -19,6 +20,7 @@ import org.eqasim.sao_paulo.mode_choice.utilities.estimators.SaoPauloPTUtilityEs
 import org.eqasim.sao_paulo.mode_choice.utilities.estimators.SaoPauloTaxiUtilityEstimator;
 import org.eqasim.sao_paulo.mode_choice.utilities.estimators.SaoPauloWalkUtilityEstimator;
 import org.eqasim.sao_paulo.mode_choice.utilities.predictors.SaoPauloPersonPredictor;
+import org.eqasim.sao_paulo.mode_choice.utilities.predictors.SaoPauloTaxiPredictor;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 
@@ -45,7 +47,7 @@ public class SaoPauloModeChoiceModule extends AbstractEqasimExtension {
 		bindModeAvailability(MODE_AVAILABILITY_NAME).to(SaoPauloModeAvailability.class);
 
 		bind(SaoPauloPersonPredictor.class);
-
+		bind(SaoPauloTaxiPredictor.class);
 		bindCostModel(CAR_COST_MODEL_NAME).to(SaoPauloCarCostModel.class);
 		bindCostModel(PT_COST_MODEL_NAME).to(SaoPauloPtCostModel.class);
 		bindCostModel(TAXI_COST_MODEL_NAME).to(SaoPauloTaxiCostModel.class);
