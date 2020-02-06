21cfe84fa3871e15144020d78497cfc54ef4e142
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-02-06 10:27:32 +0100

    Use ASTRA 2016 parameters for Zurich

---
+++ examples/src/main/java/org/eqasim/projects/thesis/ThesisZurichModule.java
@@ -0,0 +1,40 @@
+package org.eqasim.projects.thesis;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eqasim.core.components.config.EqasimConfigGroup;
+import org.eqasim.core.simulation.mode_choice.AbstractEqasimExtension;
+import org.eqasim.core.simulation.mode_choice.ParameterDefinition;
+import org.eqasim.switzerland.mode_choice.parameters.SwissModeParameters;
+import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class ThesisZurichModule extends AbstractEqasimExtension {
+	private final CommandLine commandLine;
+
+	public ThesisZurichModule(CommandLine commandLine) {
+		this.commandLine = commandLine;
+	}
+
+	@Provides
+	@Singleton
+	public SwissModeParameters provideSwissModeParameters(EqasimConfigGroup config)
+			throws IOException, ConfigurationException {
+		SwissModeParameters parameters = SwissModeParameters.buildASTRA2016();
+
+		if (config.getModeParametersPath() != null) {
+			ParameterDefinition.applyFile(new File(config.getModeParametersPath()), parameters);
+		}
+
+		ParameterDefinition.applyCommandLine("mode-parameter", commandLine, parameters);
+		return parameters;
+	}
+
+	@Override
+	protected void installEqasimExtension() {
+	}
+}
--- switzerland/src/main/java/org/eqasim/switzerland/mode_choice/parameters/SwissModeParameters.java
+++ switzerland/src/main/java/org/eqasim/switzerland/mode_choice/parameters/SwissModeParameters.java
@@ -53,4 +53,38 @@ public class SwissModeParameters extends ModeParameters {
 
 		return parameters;
 	}
+
+	public static SwissModeParameters buildASTRA2016() {
+		SwissModeParameters parameters = new SwissModeParameters();
+
+		// Cost
+		parameters.betaCost_u_MU = -0.126;
+		parameters.lambdaCostEuclideanDistance = -0.4;
+		parameters.referenceEuclideanDistance_km = 40.0;
+
+		// Car
+		parameters.car.alpha_u = 0.827;
+		parameters.car.betaTravelTime_u_min = -0.0667;
+
+		parameters.car.constantAccessEgressWalkTime_min = 6.0;
+		parameters.car.constantParkingSearchPenalty_min = 5.0;
+
+		// PT
+		parameters.pt.alpha_u = 0.0;
+		parameters.pt.betaLineSwitch_u = -0.17;
+		parameters.pt.betaInVehicleTime_u_min = -0.0192;
+		parameters.pt.betaWaitingTime_u_min = -0.0384;
+		parameters.pt.betaAccessEgressTime_u_min = -0.0804;
+
+		// Bike
+		parameters.bike.alpha_u = -0.1;
+		parameters.bike.betaTravelTime_u_min = -0.0805;
+		parameters.bike.betaAgeOver18_u_a = -0.0496;
+
+		// Walk
+		parameters.walk.alpha_u = 0.63;
+		parameters.walk.betaTravelTime_u_min = -0.141;
+
+		return parameters;
+	}
 }
