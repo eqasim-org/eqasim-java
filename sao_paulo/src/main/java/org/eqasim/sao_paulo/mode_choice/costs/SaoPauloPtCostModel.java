f771f33eb5c1667c2818196768eb8a7d2a800e94
balacmi <milos.balac@ivt.baug.ethz.ch>
2020-03-13 12:01:29 +0100

    more detailed fare structure
    people pay higher fare only if they transfer between rail/metro and bus.
    Otherwise they pay single ticket

--- sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/costs/SaoPauloPtCostModel.java
+++ sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/costs/SaoPauloPtCostModel.java
@@ -2,13 +2,17 @@ package org.eqasim.sao_paulo.mode_choice.costs;
 
 import java.util.List;
 
+import org.eqasim.core.components.transit.routing.EnrichedTransitRoute;
 import org.eqasim.core.simulation.mode_choice.cost.CostModel;
 import org.eqasim.sao_paulo.mode_choice.parameters.SaoPauloCostParameters;
 import org.eqasim.sao_paulo.mode_choice.utilities.predictors.SaoPauloPersonPredictor;
 import org.eqasim.sao_paulo.mode_choice.utilities.variables.SaoPauloPersonVariables;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
 
 import com.google.inject.Inject;
 
@@ -17,23 +21,52 @@ import ch.ethz.matsim.discrete_mode_choice.model.DiscreteModeChoiceTrip;
 public class SaoPauloPtCostModel implements CostModel {
 	private final SaoPauloPersonPredictor predictor;
 	private final SaoPauloCostParameters parameters;
+	private final Scenario scenario;
 
 	@Inject
-	public SaoPauloPtCostModel(SaoPauloCostParameters parameters, SaoPauloPersonPredictor predictor) {
+	public SaoPauloPtCostModel(SaoPauloCostParameters parameters, SaoPauloPersonPredictor predictor,
+			Scenario scenario) {
 		this.predictor = predictor;
 		this.parameters = parameters;
+		this.scenario = scenario;
 	}
-	
-	public int getNumberOfVehicles(List<? extends PlanElement> elements) {
+
+	public int getNumberOfMetroRailVehicles(List<? extends PlanElement> elements) {
 		int n_Vehicles = 0;
 		String mode = "pt";
-		
+
 		for (PlanElement element : elements) {
 			if (element instanceof Leg) {
 				Leg leg = (Leg) element;
 
 				if (leg.getMode().contentEquals(mode)) {
-					n_Vehicles += 1;
+
+					TransitLine tl = scenario.getTransitSchedule().getTransitLines()
+							.get(((EnrichedTransitRoute) leg.getRoute()).getTransitLineId());
+					TransitRoute tr = tl.getRoutes().get(((EnrichedTransitRoute) leg.getRoute()).getTransitRouteId());
+					if (tr.getTransportMode().equals("subway") || tr.getTransportMode().equals("rail"))
+						n_Vehicles += 1;
+				}
+			}
+		}
+		return n_Vehicles;
+	}
+
+	public int getNumberOfBusVehicles(List<? extends PlanElement> elements) {
+		int n_Vehicles = 0;
+		String mode = "pt";
+
+		for (PlanElement element : elements) {
+			if (element instanceof Leg) {
+				Leg leg = (Leg) element;
+
+				if (leg.getMode().contentEquals(mode)) {
+
+					TransitLine tl = scenario.getTransitSchedule().getTransitLines()
+							.get(((EnrichedTransitRoute) leg.getRoute()).getTransitLineId());
+					TransitRoute tr = tl.getRoutes().get(((EnrichedTransitRoute) leg.getRoute()).getTransitRouteId());
+					if (tr.getTransportMode().equals("bus"))
+						n_Vehicles += 1;
 				}
 			}
 		}
@@ -47,17 +80,16 @@ public class SaoPauloPtCostModel implements CostModel {
 		if (variables.hasSubscription) {
 			return 0.0;
 		}
-		
-		int n_Vehicles = getNumberOfVehicles(elements);
-		
-		if (n_Vehicles % 4 == 0) {
-			return parameters.ptCostPerTrip_3Transfers_BRL * (n_Vehicles / 4);
-		}
-		else if (n_Vehicles % 4 == 1) {
-			return parameters.ptCostPerTrip_0Transfers_BRL + parameters.ptCostPerTrip_3Transfers_BRL * (n_Vehicles / 4);
-		}
-		else {
-			return parameters.ptCostPerTrip_3Transfers_BRL * (1 + (n_Vehicles / 4));
-		}
+
+		int n_VehiclesMR = getNumberOfMetroRailVehicles(elements);
+		int n_VehiclesBus = getNumberOfBusVehicles(elements);
+
+		if (n_VehiclesBus == 0 || n_VehiclesMR == 0)
+			return parameters.ptCostPerTrip_0Transfers_BRL;
+		else if (n_VehiclesBus > 0 && n_VehiclesMR > 0)
+			return parameters.ptCostPerTrip_3Transfers_BRL;
+		else
+			return parameters.ptCostPerTrip_0Transfers_BRL;
+
 	}
 }
