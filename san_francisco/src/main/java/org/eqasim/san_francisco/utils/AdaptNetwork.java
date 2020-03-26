6e1d802b6ec277fd7c8dbd7b4693429daec6a23f
balacmi <milos.balac@ivt.baug.ethz.ch>
2020-03-26 22:01:10 +0100

    adapt unclassified roads

---
+++ san_francisco/src/main/java/org/eqasim/san_francisco/utils/AdaptNetwork.java
@@ -0,0 +1,32 @@
+package org.eqasim.san_francisco.utils;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.NetworkWriter;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class AdaptNetwork {
+
+	public static void main(String[] args) {
+
+		Config config = ConfigUtils.createConfig();
+		Scenario scenario = ScenarioUtils.createMutableScenario(config);
+		MatsimNetworkReader netReader = new MatsimNetworkReader(scenario.getNetwork());
+		netReader.readFile(args[0]);
+		
+		for (Link link : scenario.getNetwork().getLinks().values()) {
+			
+			if (link.getAttributes().getAttribute("osm:way:highway") !=null &&
+					link.getAttributes().getAttribute("osm:way:highway").equals("unclassified")) {
+				link.setCapacity(link.getCapacity() * 2);					
+			}
+		}
+		
+		new NetworkWriter(scenario.getNetwork()).write(args[1]);
+		
+	}
+
+}
