690e4f42e911ecf7cc9b80487df40562d8751799
balacmi <milos.balac@ivt.baug.ethz.ch>
2020-03-30 10:45:10 +0200

    this fix should avoid NullPointerException for truck agents

--- wayne_county/src/main/java/org/eqasim/wayne_county/mode_choice/utilities/predictors/WayneCountyPredictorUtils.java
+++ wayne_county/src/main/java/org/eqasim/wayne_county/mode_choice/utilities/predictors/WayneCountyPredictorUtils.java
@@ -5,6 +5,9 @@ import org.matsim.api.core.v01.population.Person;
 public class WayneCountyPredictorUtils {
 
 	static public int hhlIncomeClass(Person person) {
+		String income = ((String)person.getAttributes().getAttribute("income"));
+		if (income.length()<4)
+			return -1;
 		int hhlIncomeClass = (int) ((String)person.getAttributes().getAttribute("income")).charAt(3);
 		return hhlIncomeClass;
 	}
