982b2f17c643f44d62bbdb1f39b5cb0fa7021ca4
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-04-08 10:59:34 +0200

    Update cost calulcator

--- examples/src/main/java/org/eqasim/projects/astra16/pricing/model/AstraAvCostModel.java
+++ examples/src/main/java/org/eqasim/projects/astra16/pricing/model/AstraAvCostModel.java
@@ -6,22 +6,25 @@ import org.eqasim.core.simulation.mode_choice.cost.AbstractCostModel;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
 import ch.ethz.matsim.discrete_mode_choice.model.DiscreteModeChoiceTrip;
 
+@Singleton
 public class AstraAvCostModel extends AbstractCostModel {
 	static public final String NAME = "AstraAvCostModel";
-	// private final PriceInterpolator interpolator;
+	private final PriceInterpolator interpolator;
 
-	public AstraAvCostModel() { // PriceInterpolator interpolator) {
+	@Inject
+	public AstraAvCostModel(PriceInterpolator interpolator) {
 		super("av");
-		// this.interpolator = interpolator;
+		this.interpolator = interpolator;
 	}
 
 	@Override
 	public double calculateCost_MU(Person person, DiscreteModeChoiceTrip trip, List<? extends PlanElement> elements) {
-		// return interpolator.getPricePerTrip_CHF()
-		// + interpolator.getPricePerKm_CHF() * getInVehicleDistance_km(elements);
-
-		return 0.5 * getInVehicleDistance_km(elements);
+		return interpolator.getPricePerTrip_CHF()
+				+ interpolator.getPricePerKm_CHF() * getInVehicleDistance_km(elements);
 	}
 }
