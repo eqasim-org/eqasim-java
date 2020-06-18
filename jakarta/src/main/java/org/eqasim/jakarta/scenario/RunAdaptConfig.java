bb0908fe76b533849367606d3e2d8af6d800a663
ilahia <anugrah.ilahi@ivt.baug.ethz.ch>
2020-06-18 23:30:35 +0200

    commit seventh

--- jakarta/src/main/java/org/eqasim/jakarta/scenario/RunAdaptConfig.java
+++ jakarta/src/main/java/org/eqasim/jakarta/scenario/RunAdaptConfig.java
@@ -1,11 +1,15 @@
 package org.eqasim.jakarta.scenario;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.eqasim.core.components.config.ConfigAdapter;
 import org.eqasim.core.components.config.EqasimConfigGroup;
 import org.eqasim.core.simulation.EqasimConfigurator;
 import org.eqasim.jakarta.mode_choice.JakartaModeChoiceModule;
+import org.eqasim.sao_paulo.mode_choice.SaoPauloModeChoiceModule;
+import org.eqasim.sao_paulo.scenario.RunAdaptConfig;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
@@ -13,6 +17,8 @@ import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
 
 import ch.ethz.matsim.discrete_mode_choice.model.DiscreteModeChoiceModel.FallbackBehaviour;
 import ch.ethz.matsim.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
+
+publiimport ch.ethz.matsim.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
 
 public class RunAdaptConfig {
 	static public void main(String[] args) throws ConfigurationException {
@@ -27,16 +33,24 @@ public class RunAdaptConfig {
 		// Set up mode choice
 		EqasimConfigGroup eqasimConfig = EqasimConfigGroup.get(config);
 
-		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) conJJakartaModeChoiceModule(DiscreteModeChoiceConfigGroup.GROUP_N
-		eqasimConfig.setCostModel(JaJakartaModeChoiceModuleiscoModeChoiceModule.CAR_COST_MODEL_NAME);
-		eqasimConfig.setCostMoJakJakartaModeChoiceModulenciscoModeChoiceModule.PT_COST_MODEL_NAME);
-AME);
+		eqasimConfig.setCostModel(TransportMode.car, JakartaModeChoiceModule.CAR_COST_MODEL_NAME);
+		eqasimConfig.setCostModel(TransportMode.pt, JakartaModeChoiceModule.PT_COST_MODEL_NAME);
+		eqasimConfig.setCostModel(TransportMode.taxi, JakartaModeChoiceModule.TAXI_COST_MODEL_NAME);
 
-		dmcConfig.setModeAvailability(SanFranciscoModeChoiceModule.MODE_AVAILABILITY_NAME);
-		Collection<String> tripConstraints = dmcConfig.getTripConstraintJakaJakartaModeChoiceModuleWalkDurationConstraint");
+		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules()
+				.get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
+
+		dmcConfig.setModeAvailability(JakartaModeChoiceModule.MODE_AVAILABILITY_NAME);
+		Collection<String> tripConstraints = dmcConfig.getTripConstraints();
+		tripConstraints.add("WalkDurationConstraint");
 		dmcConfig.setTripConstraints(tripConstraints);
-		dmcConfig.setTourConstraintsAsString("FromTripBased, VehicleTourConstraintWithCarPassenger");
-		dmcConfig.setFallbackBehaviour(FallbackBehaviour.INITIAL_CHOICE);
-
+		
+		List<String> networkModes = new LinkedList<>(config.plansCalcRoute().getNetworkModes());
+		networkModes.add("taxi");
+		config.plansCalcRoute().setNetworkModes(networkModes);
+		
+		ModeParams taxiParams = new ModeParams("taxi");
+		config.planCalcScore().addModeParams(taxiParams);
+		
 	}
 }
