dc54d959c79d44b8ed13fa2461623c914bce7e22
ilahia <anugrah.ilahi@ivt.baug.ethz.ch>
2020-06-18 23:28:19 +0200

    commit sixth

--- jakarta/src/main/java/org/eqasim/jakarta/scenario/RunAdaptConfig.java
+++ jakarta/src/main/java/org/eqasim/jakarta/scenario/RunAdaptConfig.java
@@ -1,18 +1,17 @@
 package org.eqasim.jakarta.scenario;
 
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.eqasim.core.components.config.ConfigAdapter;
 import org.eqasim.core.components.config.EqasimConfigGroup;
 import org.eqasim.core.simulation.EqasimConfigurator;
-import org.eqasim.jakarta.mode_choice.SaoPauloModeChoiceModule;
+import org.eqasim.jakarta.mode_choice.JakartaModeChoiceModule;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
 
+import ch.ethz.matsim.discrete_mode_choice.model.DiscreteModeChoiceModel.FallbackBehaviour;
 import ch.ethz.matsim.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
 
 public class RunAdaptConfig {
@@ -28,24 +27,16 @@ public class RunAdaptConfig {
 		// Set up mode choice
 		EqasimConfigGroup eqasimConfig = EqasimConfigGroup.get(config);
 
-		eqasimConfig.setCostModel(TransportMode.car, SaoPauloModeChoiceModule.CAR_COST_MODEL_NAME);
-		eqasimConfig.setCostModel(TransportMode.pt, SaoPauloModeChoiceModule.PT_COST_MODEL_NAME);
-		eqasimConfig.setCostModel(TransportMode.taxi, SaoPauloModeChoiceModule.TAXI_COST_MODEL_NAME);
+		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) conJJakartaModeChoiceModule(DiscreteModeChoiceConfigGroup.GROUP_N
+		eqasimConfig.setCostModel(JaJakartaModeChoiceModuleiscoModeChoiceModule.CAR_COST_MODEL_NAME);
+		eqasimConfig.setCostMoJakJakartaModeChoiceModulenciscoModeChoiceModule.PT_COST_MODEL_NAME);
+AME);
 
-		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules()
-				.get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
-
-		dmcConfig.setModeAvailability(SaoPauloModeChoiceModule.MODE_AVAILABILITY_NAME);
-		Collection<String> tripConstraints = dmcConfig.getTripConstraints();
-		tripConstraints.add("WalkDurationConstraint");
+		dmcConfig.setModeAvailability(SanFranciscoModeChoiceModule.MODE_AVAILABILITY_NAME);
+		Collection<String> tripConstraints = dmcConfig.getTripConstraintJakaJakartaModeChoiceModuleWalkDurationConstraint");
 		dmcConfig.setTripConstraints(tripConstraints);
-		
-		List<String> networkModes = new LinkedList<>(config.plansCalcRoute().getNetworkModes());
-		networkModes.add("taxi");
-		config.plansCalcRoute().setNetworkModes(networkModes);
-		
-		ModeParams taxiParams = new ModeParams("taxi");
-		config.planCalcScore().addModeParams(taxiParams);
-		
+		dmcConfig.setTourConstraintsAsString("FromTripBased, VehicleTourConstraintWithCarPassenger");
+		dmcConfig.setFallbackBehaviour(FallbackBehaviour.INITIAL_CHOICE);
+
 	}
 }
