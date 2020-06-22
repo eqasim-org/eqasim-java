b3fe3dee73b520c1a46eff552db61a50a27b951d
ilahia <anugrah.ilahi@ivt.baug.ethz.ch>
2020-06-22 18:34:08 +0200

    fist commit

--- jakarta/src/main/java/org/eqasim/jakarta/RunSimulation.java
+++ jakarta/src/main/java/org/eqasim/jakarta/RunSimulation.java
@@ -34,7 +34,7 @@ public class RunSimulation {
 		eqasimConfig.setEstimator("walk", "spWalkEstimator");
 		eqasimConfig.setEstimator("pt", "spPTEstimator");
 		eqasimConfig.setEstimator("car", "spCarEstimator");
-		eqasimConfig.setEstimator("taxi", "spTaxiEstimator");		
+		eqasimConfig.setEstimator("taxi", "spTaxiEstimators");		
 
 		Controler controller = new Controler(scenario);
 		EqasimConfigurator.configureController(controller);
