844c3445fd0ca0d929660ec319bf9c282c2180af
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-09-02 14:40:42 +0200

    Add capacity config back in

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
@@ -5,6 +5,7 @@ import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
 import org.eqasim.ile_de_france.grand_paris.PersonUtilityModule;
 import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
@@ -16,7 +17,7 @@ public class RunSimulation {
 	static public void main(String[] args) throws ConfigurationException {
 		CommandLine cmd = new CommandLine.Builder(args) //
 				.requireOptions("config-path") //
-				.allowOptions("model") //
+				.allowOptions("model", "urban-factor") //
 				.allowPrefixes("mode-choice-parameter", "cost-parameter") //
 				.build();
 
@@ -27,6 +28,18 @@ public class RunSimulation {
 		Scenario scenario = ScenarioUtils.createScenario(config);
 		IDFConfigurator.configureScenario(scenario);
 		ScenarioUtils.loadScenario(scenario);
+
+		{
+			double factor = cmd.getOption("urban-factor").map(Double::parseDouble).orElse(0.8);
+
+			for (Link link : scenario.getNetwork().getLinks().values()) {
+				Boolean isUrban = (Boolean) link.getAttributes().getAttribute("isUrban");
+
+				if (isUrban != null && isUrban) {
+					link.setCapacity(factor * link.getCapacity() / 0.8);
+				}
+			}
+		}
 
 		Controler controller = new Controler(scenario);
 		IDFConfigurator.configureController(controller);
