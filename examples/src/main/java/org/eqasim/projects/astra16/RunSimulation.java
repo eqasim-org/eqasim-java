03d03edb44bbb8f6b73587d3e23a4e1a9d61ccf2
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-10-01 20:00:43 +0200

    Move crossing penalty to network

--- examples/src/main/java/org/eqasim/projects/astra16/RunSimulation.java
+++ examples/src/main/java/org/eqasim/projects/astra16/RunSimulation.java
@@ -1,5 +1,7 @@
 package org.eqasim.projects.astra16;
 
+import org.eqasim.core.components.config.EqasimConfigGroup;
+import org.eqasim.core.components.traffic.EqasimLinkSpeedCalculator;
 import org.eqasim.core.simulation.analysis.EqasimAnalysisModule;
 import org.eqasim.core.simulation.calibration.CalibrationModule;
 import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
@@ -7,12 +9,20 @@ import org.eqasim.projects.astra16.travel_time.TravelTimeComparisonModule;
 import org.eqasim.switzerland.SwitzerlandConfigurator;
 import org.eqasim.switzerland.mode_choice.SwissModeChoiceModule;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.mobsim.qsim.qnetsimengine.ConfigurableQNetworkFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetworkFactory;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class RunSimulation {
 	static public void main(String[] args) throws ConfigurationException {
@@ -33,6 +43,25 @@ public class RunSimulation {
 		SwitzerlandConfigurator.adjustScenario(scenario);
 		AstraConfigurator.adjustScenario(scenario);
 
+		EqasimConfigGroup eqasimConfig = EqasimConfigGroup.get(config);
+
+		for (Link link : scenario.getNetwork().getLinks().values()) {
+			double maximumSpeed = link.getFreespeed();
+			boolean isMajor = true;
+
+			for (Link other : link.getToNode().getInLinks().values()) {
+				if (other.getCapacity() >= link.getCapacity()) {
+					isMajor = false;
+				}
+			}
+
+			if (!isMajor && link.getToNode().getInLinks().size() > 1) {
+				double travelTime = link.getLength() / maximumSpeed;
+				travelTime += eqasimConfig.getCrossingPenalty();
+				link.setFreespeed(link.getLength() / travelTime);
+			}
+		}
+
 		Controler controller = new Controler(scenario);
 		SwitzerlandConfigurator.configureController(controller);
 		controller.addOverridingModule(new EqasimAnalysisModule());
@@ -42,6 +71,20 @@ public class RunSimulation {
 		controller.addOverridingModule(new AstraModule(cmd));
 		controller.addOverridingModule(new TravelTimeComparisonModule());
 		AstraConfigurator.configureController(controller, cmd);
+
+		controller.addOverridingModule(new AbstractModule() {
+			@Provides
+			@Singleton
+			public QNetworkFactory provideQNetworkFactory(EventsManager events, Scenario scenario,
+					EqasimLinkSpeedCalculator linkSpeedCalculator) {
+				ConfigurableQNetworkFactory networkFactory = new ConfigurableQNetworkFactory(events, scenario);
+				return networkFactory;
+			}
+
+			@Override
+			public void install() {
+			}
+		});
 
 		controller.run();
 	}
