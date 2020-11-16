bdde758b8061add9a9e2e5512d42b59ece5542f9
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-11-16 13:23:38 +0100

    Only pt simulation

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
@@ -1,9 +1,14 @@
 package org.eqasim.ile_de_france;
 
+import java.util.HashSet;
+
+import org.eqasim.core.components.config.EqasimConfigGroup;
 import org.eqasim.core.simulation.analysis.EqasimAnalysisModule;
 import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
 import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
@@ -21,9 +26,15 @@ public class RunSimulation {
 		Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"), IDFConfigurator.getConfigGroups());
 		cmd.applyConfiguration(config);
 
+		EqasimConfigGroup.get(config).setUseScheduleBasedTransport(false);
+
 		Scenario scenario = ScenarioUtils.createScenario(config);
 		IDFConfigurator.configureScenario(scenario);
 		ScenarioUtils.loadScenario(scenario);
+
+		for (Id<Person> personId : new HashSet<>(scenario.getPopulation().getPersons().keySet())) {
+			scenario.getPopulation().removePerson(personId);
+		}
 
 		Controler controller = new Controler(scenario);
 		IDFConfigurator.configureController(controller);
