ac3928418b3743def498ecaab0d8b0f9406d54df
balacmi <milos.balac@ivt.baug.ethz.ch>
2021-01-25 17:24:55 +0100

    adapted the config adapter
    walk duration constraint is included
    business activity type is added
    fallback should be initial choice

--- los_angeles/src/main/java/org/eqasim/los_angeles/scenario/RunAdaptConfig.java
+++ los_angeles/src/main/java/org/eqasim/los_angeles/scenario/RunAdaptConfig.java
@@ -1,15 +1,24 @@
 package org.eqasim.los_angeles.scenario;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.eqasim.core.components.config.ConfigAdapter;
 import org.eqasim.core.components.config.EqasimConfigGroup;
 import org.eqasim.core.simulation.EqasimConfigurator;
 import org.eqasim.los_angeles.mode_choice.LosAngelesModeChoiceModule;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.contribs.discrete_mode_choice.model.DiscreteModeChoiceModel.FallbackBehaviour;
 import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
 import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.Config;
 
 public class RunAdaptConfig {
+	protected final static List<String> ACTIVITY_TYPES = Arrays.asList("business");
+
 	static public void main(String[] args) throws ConfigurationException {
 		ConfigAdapter.run(args, EqasimConfigurator.getConfigGroups(), RunAdaptConfig::adaptConfiguration);
 	}
@@ -27,8 +36,23 @@ public class RunAdaptConfig {
 
 		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules()
 				.get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
-
+		Collection<String> tripConstraints = dmcConfig.getTripConstraints();
+		tripConstraints.add("WalkDurationConstraint");
+		dmcConfig.setTripConstraints(tripConstraints);
 		dmcConfig.setModeAvailability(LosAngelesModeChoiceModule.MODE_AVAILABILITY_NAME);
 		dmcConfig.setTourConstraintsAsString("FromTripBased, VehicleTourConstraintWithCarPassenger");
+		dmcConfig.setFallbackBehaviour(FallbackBehaviour.INITIAL_CHOICE);
+		PlanCalcScoreConfigGroup scoringConfig = config.planCalcScore();
+
+		for (String activityType : ACTIVITY_TYPES) {
+			ActivityParams activityParams = scoringConfig.getActivityParams(activityType);
+
+			if (activityParams == null) {
+				activityParams = new ActivityParams(activityType);
+				config.planCalcScore().addActivityParams(activityParams);
+			}
+
+			activityParams.setScoringThisActivityAtAll(false);
+		}
 	}
 }
