80bb73a1eda13e124440da0fea52d24815d06268
Milos Balac <milos.balac@ivt.baug.ethz.ch>
2021-03-02 10:12:44 +0100

    Update SaoPauloCostParameters.java
    adjust taxi cost based on assumed occupancy of 2

--- sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/parameters/SaoPauloCostParameters.java
+++ sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/parameters/SaoPauloCostParameters.java
@@ -18,15 +18,17 @@ public class SaoPauloCostParameters implements ParameterDefinition {
 	public static SaoPauloCostParameters buildDefault() {
 		SaoPauloCostParameters parameters = new SaoPauloCostParameters();
 
-		parameters.carCost_BRL_km = 0.51;
+		parameters.carCost_BRL_km = 0.35;
 		
 		parameters.ptCostPerTrip_0Transfers_BRL = 4.3;
 		parameters.ptCostPerTrip_3Transfers_BRL = 7.48;
 		
-		parameters.taxiPickUpFee_BRL = 3.25;//4.16;
-		parameters.taxiCostPerMin_BRL = 0.38;//0.48;
-		parameters.taxiCostPerkm_BRL = 2.0;//2.4;
-		parameters.taxMinCost_BRL = 7.0;//2.4;
+		// it is assumed a 2 person occupancy of taxi services
+		parameters.taxiPickUpFee_BRL = 2.08;//4.16;
+		parameters.taxiCostPerMin_BRL = 0.24;//0.48;
+		parameters.taxiCostPerkm_BRL = 1.2;//2.4;
+		parameters.taxMinCost_BRL = 7.0;
+
 
 		return parameters;
 	}
