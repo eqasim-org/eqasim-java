4267f906b4f8cd94fd195598f97e9434e4cb4ff4
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-08-04 13:34:24 +0200

    Add vdf module

---
+++ vdf/src/main/java/org/eqasim/vdf/VDFQSimModule.java
@@ -0,0 +1,32 @@
+package org.eqasim.vdf;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.mobsim.qsim.AbstractQSimModule;
+import org.matsim.core.mobsim.qsim.qnetsimengine.ConfigurableQNetworkFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetworkFactory;
+
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class VDFQSimModule extends AbstractQSimModule {
+	@Override
+	protected void configureQSim() {
+	}
+
+	@Provides
+	@Singleton
+	public QNetworkFactory provideQNetworkFactory(EventsManager events, Scenario scenario,
+			VDFLinkSpeedCalculator linkSpeedCalculator) {
+		ConfigurableQNetworkFactory networkFactory = new ConfigurableQNetworkFactory(events, scenario);
+		networkFactory.setLinkSpeedCalculator(linkSpeedCalculator);
+		return networkFactory;
+	}
+
+	@Provides
+	@Singleton
+	public VDFLinkSpeedCalculator provideVDFLinkSpeedCalculator(Population population, VDFTravelTime travelTime) {
+		return new VDFLinkSpeedCalculator(population, travelTime);
+	}
+}
