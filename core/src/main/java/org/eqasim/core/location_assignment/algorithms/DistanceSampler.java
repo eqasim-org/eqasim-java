af25affeea0f75962038aa01246373783bb6b01a
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-08-17 10:27:52 +0200

    Remove location assignment functionality. Closes #70.

--- core/src/main/java/org/eqasim/core/location_assignment/algorithms/DistanceSampler.java
+++
@@ -1,5 +0,0 @@
-package org.eqasim.core.location_assignment.algorithms;
-
-public interface DistanceSampler {
-	double sample();
-}
--- core/src/main/java/org/eqasim/core/location_assignment/algorithms/discretizer/DiscretizerResult.java
+++
@@ -1,7 +0,0 @@
-package org.eqasim.core.location_assignment.algorithms.discretizer;
-
-import org.eqasim.core.location_assignment.assignment.discretization.DiscreteLocation;
-
-public interface DiscretizerResult {
-	DiscreteLocation getLocation();
-}
