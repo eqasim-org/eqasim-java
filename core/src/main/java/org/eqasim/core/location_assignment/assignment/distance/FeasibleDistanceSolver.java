9aed6afd23da344a384eb612caba9f2582fa2c0b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-08-17 10:36:32 +0200

    Remove location assignment functionality. Closes #70. (#108)

--- core/src/main/java/org/eqasim/core/location_assignment/assignment/distance/FeasibleDistanceSolver.java
+++
@@ -1,7 +0,0 @@
-package org.eqasim.core.location_assignment.assignment.distance;
-
-import org.eqasim.core.location_assignment.assignment.LocationAssignmentProblem;
-
-public interface FeasibleDistanceSolver {
-	FeasibleDistanceResult solve(LocationAssignmentProblem problem);
-}
