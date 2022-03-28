8b51a6d43ad1c0f22f64c9012d76fc12da185a76
Valentin LE BESCOND <valentin.lebescond@protonmail.com>
2022-03-28 17:32:09 +0200

    optimize imports

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
@@ -3,20 +3,13 @@ package org.eqasim.ile_de_france;
 import org.eqasim.core.simulation.analysis.EqasimAnalysisModule;
 import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
 import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.vehicles.Vehicle;
-import org.matsim.vehicles.VehicleUtils;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class RunSimulation {
 	static public void main(String[] args) throws ConfigurationException {
