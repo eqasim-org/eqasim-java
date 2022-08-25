0710336ba8abcae6cb2001aa42c3fc1b50549989
Sebastian Hörl <hoerl.sebastian@gmail.com>
2022-08-25 10:42:59 +0200

    feat: Add stop area to transit leg analysis output

--- core/src/main/java/org/eqasim/core/analysis/pt/PublicTransportLegItem.java
+++ core/src/main/java/org/eqasim/core/analysis/pt/PublicTransportLegItem.java
@@ -4,6 +4,7 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.pt.transitSchedule.api.TransitLine;
 import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitStopArea;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
 public class PublicTransportLegItem {
@@ -17,9 +18,12 @@ public class PublicTransportLegItem {
 	public Id<TransitLine> transitLineId;
 	public Id<TransitRoute> transitRouteId;
 
+	public Id<TransitStopArea> accessAreaId;
+	public Id<TransitStopArea> egressAreaId;
+
 	public PublicTransportLegItem(Id<Person> personId, int personTripId, int legIndex,
 			Id<TransitStopFacility> accessStopId, Id<TransitStopFacility> egressStopId, Id<TransitLine> transitLineId,
-			Id<TransitRoute> transitRouteId) {
+			Id<TransitRoute> transitRouteId, Id<TransitStopArea> accessAreaId, Id<TransitStopArea> egressAreaId) {
 		this.personId = personId;
 		this.personTripId = personTripId;
 		this.legIndex = legIndex;
@@ -29,5 +33,8 @@ public class PublicTransportLegItem {
 
 		this.transitLineId = transitLineId;
 		this.transitRouteId = transitRouteId;
+
+		this.accessAreaId = accessAreaId;
+		this.egressAreaId = egressAreaId;
 	}
 }
