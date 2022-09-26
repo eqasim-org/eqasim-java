553547f4f9353f88eb2c7900aa50b84726567396
Gladys Gan <gladysmcgan@gmail.com>
2022-09-26 14:31:07 +0200

    updated parameters to MATSim model with gradient variable for bike

--- switzerland/src/main/java/org/eqasim/switzerland/mode_choice/parameters/SwissModeParameters.java
+++ switzerland/src/main/java/org/eqasim/switzerland/mode_choice/parameters/SwissModeParameters.java
@@ -71,12 +71,12 @@ public class SwissModeParameters extends ModeParameters {
 		parameters.lambdaCostEuclideanDistance = -0.0; //g/ not used
 		parameters.referenceEuclideanDistance_km = 0.0; //g/ not used
 
-		parameters.betaCost_RD = -0.711;
-		parameters.lambdaCostRoutedDistance = -0.97;
+		parameters.betaCost_RD = -0.712;
+		parameters.lambdaCostRoutedDistance = -0.964;
 		parameters.referenceRoutedDistance_km = 10.0;
 
 		// Car
-		parameters.car.alpha_u = 1.83;
+		parameters.car.alpha_u = 1.8;
 		parameters.car.betaTravelTime_u_min = -0.0; //g/ not used
 
 		parameters.car.constantAccessEgressWalkTime_min = 0.0; //g/ not used
@@ -85,60 +85,60 @@ public class SwissModeParameters extends ModeParameters {
 		parameters.swissCar.betaStatedPreferenceRegion1_u = -0.0; //g/ not used
 		parameters.swissCar.betaStatedPreferenceRegion3_u = 0.0; //g/ not used
 
-		parameters.swissCar.betaAge = 0.00736;
-		parameters.swissCar.betaIsFemale = -0.192;
-		parameters.swissCar.betaIsWorkTrip = -0.246;
-		parameters.swissCar.betaTravelTime_hour = -4.67;
+		parameters.swissCar.betaAge = 0.00752;
+		parameters.swissCar.betaIsFemale = -0.193;
+		parameters.swissCar.betaIsWorkTrip = -0.255;
+		parameters.swissCar.betaTravelTime_hour = -4.68;
 
 
 		// PT
 		parameters.pt.alpha_u = 0.0;
-		parameters.pt.betaLineSwitch_u = -0.0; //<---change
+		parameters.pt.betaLineSwitch_u = -0.2; //this is "transfers"
 		parameters.pt.betaInVehicleTime_u_min = -0.0; //g/ not used
 		parameters.pt.betaWaitingTime_u_min = -0.0; //g/ not used
 		parameters.pt.betaAccessEgressTime_u_min = -0.0; //g/ not used
 
-		parameters.swissPt.betaAge = -0.00642;
-		parameters.swissPt.betaIsFemale = 0.233;
-		parameters.swissPt.betaIsWorkTrip = 0.186;
+		parameters.swissPt.betaAge = -0.00627;
+		parameters.swissPt.betaIsFemale = 0.23;
+		parameters.swissPt.betaIsWorkTrip = 0.203;
 
-		parameters.swissPt.betaAccessEgressTime_hour =-1.66;
-		parameters.swissPt.betaInVehicleTime_hour =-2.47;
-		parameters.swissPt.betaWaitingTime_hour =-1.19;//<---change
+		parameters.swissPt.betaAccessEgressTime_hour =-1.76;
+		parameters.swissPt.betaInVehicleTime_hour =-2.36;
+		parameters.swissPt.betaWaitingTime_hour =-0.354; //this is "transferTime"
 
 		// Bike
-		parameters.bike.alpha_u = -0.684;
+		parameters.bike.alpha_u = -0.707;
 		parameters.bike.betaTravelTime_u_min = -0.0; //g/ not used
 		parameters.bike.betaAgeOver18_u_a = -0.0; //g/ not used
 		parameters.swissBike.betaStatedPreferenceRegion3_u = -0.0; //g/ not used
 
-		parameters.swissBike.betaPropS1L1 = 0.206;
-		parameters.swissBike.betaPropS2L1 = 0.000514;
-		parameters.swissBike.betaPropS3L1 = -0.447;
-		parameters.swissBike.betaPropS4L1 = -1.44;
-		parameters.swissBike.betaPropS1L2 = -0.452;
-		parameters.swissBike.betaPropS2L2 = -1.49;
-		parameters.swissBike.betaPropS3L2 = -3.33;
-		parameters.swissBike.betaPropS4L2 = -1.29;
+		parameters.swissBike.betaPropS1L1 = 0.345;
+		parameters.swissBike.betaPropS2L1 = 0.169;
+		parameters.swissBike.betaPropS3L1 = -0.363;
+		parameters.swissBike.betaPropS4L1 = -1.25;
+		parameters.swissBike.betaPropS1L2 = 0.0979;
+		parameters.swissBike.betaPropS2L2 = -1.07;
+		parameters.swissBike.betaPropS3L2 = -2.89;
+		parameters.swissBike.betaPropS4L2 = -0.853;
 		parameters.swissBike.lambdaRCD = 0.0; //g/ no interaction term, use as 0
 		parameters.swissBike.referenceRoutedDistance_km = 0.0; //g/ no interaction term, use as 0
 
-		parameters.swissBike.betaAverageUphillGradient = 0.0; //not used here yet
-		parameters.swissBike.lambdaGD = 0.0; //not used here yet
+		parameters.swissBike.betaAverageUphillGradient = -9.95;
+		parameters.swissBike.lambdaGD = 0.751;
 
-		parameters.swissBike.betaAge = -0.000618;
-		parameters.swissBike.betaIsFemale = -0.0794;
-		parameters.swissBike.betaIsWorkTrip = 0.465;
-		parameters.swissBike.betaTravelTime_hour = -6.28;
+		parameters.swissBike.betaAge = -0.000914;
+		parameters.swissBike.betaIsFemale = -0.0768;
+		parameters.swissBike.betaIsWorkTrip = 0.46;
+		parameters.swissBike.betaTravelTime_hour = -5.26;
 
 		// Walk
-		parameters.walk.alpha_u = 1.97;
+		parameters.walk.alpha_u = 2.0;
 		parameters.walk.betaTravelTime_u_min = -0.0; //g/ not used
 
-		parameters.swissWalk.betaAge = -0.00032;
-		parameters.swissWalk.betaIsFemale = 0.039;
-		parameters.swissWalk.betaIsWorkTrip = -0.406;
-		parameters.swissWalk.betaTravelTime_hour = -8.27;
+		parameters.swissWalk.betaAge = -0.000337;
+		parameters.swissWalk.betaIsFemale = 0.0397;
+		parameters.swissWalk.betaIsWorkTrip = -0.409;
+		parameters.swissWalk.betaTravelTime_hour = -8.36;
 
 		return parameters;
 	}
