d600081f3f69ad17fb641b97b12ac6454fa64e3b
tchervec <christopher.tchervenkov@ivt.baug.ethz.ch>
2022-10-10 17:54:08 +0200

    Add replanning strategies for freigh subpopulation in ConfigAdapter

--- switzerland/src/main/java/org/eqasim/switzerland/scenario/RunAdaptConfig.java
+++ switzerland/src/main/java/org/eqasim/switzerland/scenario/RunAdaptConfig.java
@@ -8,6 +8,7 @@ import org.matsim.api.core.v01.TransportMode;
 import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
 import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.StrategyConfigGroup;
 
 public class RunAdaptConfig {
 	static public void main(String[] args) throws ConfigurationException {
@@ -15,6 +16,22 @@ public class RunAdaptConfig {
 	}
 
 	static public void adaptConfiguration(Config config) {
+		// add replanning strategies for freight agents
+		StrategyConfigGroup.StrategySettings freightReRouteStrategy = new StrategyConfigGroup.StrategySettings();
+		freightReRouteStrategy.setDisableAfter(-1);
+		freightReRouteStrategy.setStrategyName("ReRoute");
+		freightReRouteStrategy.setSubpopulation("freight");
+		freightReRouteStrategy.setWeight(0.05);
+		config.strategy().addStrategySettings(freightReRouteStrategy);
+
+		StrategyConfigGroup.StrategySettings freightKeepLastSelectedStrategy = new StrategyConfigGroup.StrategySettings();
+		freightKeepLastSelectedStrategy.setDisableAfter(-1);
+		freightKeepLastSelectedStrategy.setStrategyName("KeepLastSelected");
+		freightKeepLastSelectedStrategy.setSubpopulation("freight");
+		freightKeepLastSelectedStrategy.setWeight(0.95);
+		config.strategy().addStrategySettings(freightKeepLastSelectedStrategy);
+
+		// adapt eqasim config
 		EqasimConfigGroup eqasimConfig = EqasimConfigGroup.get(config);
 
 		eqasimConfig.setEstimator(TransportMode.car, SwissModeChoiceModule.CAR_ESTIMATOR_NAME);
@@ -23,6 +40,7 @@ public class RunAdaptConfig {
 		eqasimConfig.setCostModel(TransportMode.car, SwissModeChoiceModule.CAR_COST_MODEL_NAME);
 		eqasimConfig.setCostModel(TransportMode.pt, SwissModeChoiceModule.PT_COST_MODEL_NAME);
 
+		// adapt discrete mode choice config
 		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules()
 				.get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
 
