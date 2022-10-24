884ec481d6a2b9561b6e55117103d221182740a2
tchervec <christopher.tchervenkov@ivt.baug.ethz.ch>
2022-10-24 15:00:25 +0200

    correctly reset all maps on handler reset

--- examples/src/main/java/org/eqasim/examples/zurich_parking/parking/ParkingListener.java
+++ examples/src/main/java/org/eqasim/examples/zurich_parking/parking/ParkingListener.java
@@ -105,7 +105,6 @@ public class ParkingListener implements StartParkingSearchEventHandler, LinkLeav
             }
         } else {
             if (personIdParkingSearchTime.containsKey(personId)) {
-
                 // extract relevant search variables
                 double parkingSearchTime = personIdParkingSearchTime.remove(personId);
                 double parkingSearchDistance = personIdParkingSearchDistance.remove(personId);
@@ -159,6 +158,14 @@ public class ParkingListener implements StartParkingSearchEventHandler, LinkLeav
 
     @Override
     public void reset(int iteration) {
+        // clear all person maps
+        personIdParkingSearchStart.clear();
+        personIdParkingSearchTime.clear();
+        personIdParkingSearchDistance.clear();
+        personIdParkingTime.clear();
+        personIdParkingCoord.clear();
+
+        // clear all node maps
         parkingSearchTimes.clear();
         parkingSearchDistances.clear();
         egressTimes.clear();
