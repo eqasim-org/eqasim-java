6d4cec00a3b2dcfb590109d53becabfe9bcd9716
tchervec <christopher.tchervenkov@ivt.baug.ethz.ch>
2022-11-02 16:15:20 +0100

    update from IdF to Swiss mode choice parameters

--- examples/src/main/java/org/eqasim/examples/zurich_parking/mode_choice/parameters/ZurichParkingModeParameters.java
+++ examples/src/main/java/org/eqasim/examples/zurich_parking/mode_choice/parameters/ZurichParkingModeParameters.java
@@ -1,8 +1,8 @@
 package org.eqasim.examples.zurich_parking.mode_choice.parameters;
 
-import org.eqasim.ile_de_france.mode_choice.parameters.IDFModeParameters;
+import org.eqasim.switzerland.mode_choice.parameters.SwissModeParameters;
 
-public class ZurichParkingModeParameters extends IDFModeParameters {
+public class ZurichParkingModeParameters extends SwissModeParameters {
 	public class CarParkingParameters {
 		public double betaParkingSearchTime_u_min;
 		public double betaParkingCost_u_MU;
@@ -14,38 +14,54 @@ public class ZurichParkingModeParameters extends IDFModeParameters {
 		ZurichParkingModeParameters parameters = new ZurichParkingModeParameters();
 
 		// Cost
-		parameters.betaCost_u_MU = -0.206;
+		parameters.betaCost_u_MU = -0.126;
 		parameters.lambdaCostEuclideanDistance = -0.4;
 		parameters.referenceEuclideanDistance_km = 40.0;
 
 		// Car
-		parameters.car.alpha_u = 1.35;
-		parameters.car.betaTravelTime_u_min = -0.06;
+		parameters.car.alpha_u = 0.827;
+		parameters.car.betaTravelTime_u_min = -0.067;
 
-		parameters.idfCar.betaInsideUrbanArea = -0.5;
-		parameters.idfCar.betaCrossingUrbanArea = -1.0;
+		parameters.car.constantAccessEgressWalkTime_min = 4.0;
+		parameters.car.constantParkingSearchPenalty_min = 4.0;
+
+//		Sebastian thesis version
+//		parameters.car.constantAccessEgressWalkTime_min = 5.0;
+//		parameters.car.constantParkingSearchPenalty_min = 6.0;
 
 		// Car parking parameters
-		parameters.carParking.betaParkingSearchTime_u_min = -0.06;
-		parameters.carParking.betaParkingCost_u_MU = -0.206;
+		parameters.carParking.betaParkingSearchTime_u_min = -0.067;
+		parameters.carParking.betaParkingCost_u_MU = -0.126;
+
+		parameters.swissCar.betaStatedPreferenceRegion1_u = -0.4;
+		parameters.swissCar.betaStatedPreferenceRegion3_u = 0.4;
 
 		// PT
 		parameters.pt.alpha_u = 0.0;
 		parameters.pt.betaLineSwitch_u = -0.17;
-		parameters.pt.betaInVehicleTime_u_min = -0.017;
-		parameters.pt.betaWaitingTime_u_min = -0.0484;
-		parameters.pt.betaAccessEgressTime_u_min = -0.0804;
+		parameters.pt.betaInVehicleTime_u_min = -0.019;
+		parameters.pt.betaWaitingTime_u_min = -0.038;
+		parameters.pt.betaAccessEgressTime_u_min = -0.08;
 
 		// Bike
-		parameters.bike.alpha_u = -2.0;
-		parameters.bike.betaTravelTime_u_min = -0.05;
-		parameters.bike.betaAgeOver18_u_a = -0.0496;
+		parameters.bike.alpha_u = 0.344;
+		parameters.bike.betaTravelTime_u_min = -0.09;
+		parameters.bike.betaAgeOver18_u_a = -0.049;
 
-		parameters.idfBike.betaInsideUrbanArea = 1.5;
+//		// Bike: Sebastian thesis version
+//		parameters.bike.alpha_u = -0.1;
+//		parameters.bike.betaTravelTime_u_min = -0.081;
+//		parameters.bike.betaAgeOver18_u_a = -0.049;
+
+		parameters.swissBike.betaStatedPreferenceRegion3_u = -0.366;
 
 		// Walk
-		parameters.walk.alpha_u = 1.43;
-		parameters.walk.betaTravelTime_u_min = -0.15;
+		parameters.walk.alpha_u = 1.3;
+		parameters.walk.betaTravelTime_u_min = -0.141;
+
+//		// Walk: Sebastian thesis version
+//		parameters.walk.alpha_u = 0.63;
+//		parameters.walk.betaTravelTime_u_min = -0.141;
 
 		return parameters;
 	}
