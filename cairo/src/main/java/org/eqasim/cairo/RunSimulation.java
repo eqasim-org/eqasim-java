65f8dd628fa1d86bd9be495584649211a0e4c038
Sebastian Hörl <hoerl.sebastian@gmail.com>
2023-07-07 19:03:42 +0200

    Add Cairo branch

---
+++ cairo/src/main/java/org/eqasim/cairo/RunSimulation.java
@@ -0,0 +1,38 @@
+package org.eqasim.cairo;
+
+import org.eqasim.core.simulation.analysis.EqasimAnalysisModule;
+import org.eqasim.core.simulation.mode_choice.EqasimModeChoiceModule;
+import org.eqasim.ile_de_france.IDFConfigurator;
+import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class RunSimulation {
+	static public void main(String[] args) throws ConfigurationException {
+		CommandLine cmd = new CommandLine.Builder(args) //
+				.requireOptions("config-path") //
+				.allowPrefixes("mode-choice-parameter", "cost-parameter") //
+				.build();
+
+		IDFConfigurator configurator = new IDFConfigurator();
+		Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"), configurator.getConfigGroups());
+		cmd.applyConfiguration(config);
+
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		configurator.configureScenario(scenario);
+		ScenarioUtils.loadScenario(scenario);
+		configurator.adjustScenario(scenario);
+
+		Controler controller = new Controler(scenario);
+		configurator.configureController(controller);
+		controller.addOverridingModule(new EqasimAnalysisModule());
+		controller.addOverridingModule(new EqasimModeChoiceModule());
+		controller.addOverridingModule(new IDFModeChoiceModule(cmd));
+		controller.run();
+	}
+}
---
+++ cairo/src/main/java/org/eqasim/cairo/scenario/RunAdaptConfig.java
@@ -0,0 +1,25 @@
+package org.eqasim.cairo.scenario;
+
+import java.util.Arrays;
+
+import org.eqasim.core.components.config.ConfigAdapter;
+import org.eqasim.ile_de_france.IDFConfigurator;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
+
+public class RunAdaptConfig {
+	static public void main(String[] args) throws ConfigurationException {
+		IDFConfigurator configurator = new IDFConfigurator();
+		ConfigAdapter.run(args, configurator.getConfigGroups(), RunAdaptConfig::adaptConfiguration);
+	}
+
+	static public void adaptConfiguration(Config config) {
+		org.eqasim.ile_de_france.scenario.RunAdaptConfig.adaptConfiguration(config);
+
+		for (String activityType : Arrays.asList("work", "personal", "primary", "secondary", "uni", "shopping")) {
+			ActivityParams activityParams = new ActivityParams(activityType);
+			config.planCalcScore().addActivityParams(activityParams);
+		}
+	}
+}
