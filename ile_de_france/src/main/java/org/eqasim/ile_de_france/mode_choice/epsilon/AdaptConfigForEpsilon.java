4ea1bcdf2c39724cdf21910cb0a2367d9f81cc21
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2023-09-23 17:21:04 +0200

    Added AdaptConfigForEpsilon

---
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/epsilon/AdaptConfigForEpsilon.java
@@ -0,0 +1,31 @@
+package org.eqasim.ile_de_france.mode_choice.epsilon;
+
+import org.eqasim.core.components.config.EqasimConfigGroup;
+import org.matsim.contribs.discrete_mode_choice.modules.SelectorModule;
+import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
+import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+
+import java.util.Map;
+
+public class AdaptConfigForEpsilon {
+
+    public static void main(String[] args) throws CommandLine.ConfigurationException {
+        CommandLine commandLine  = new CommandLine.Builder(args).requireOptions("input-config-path", "output-config-path").build();
+
+        Config config = ConfigUtils.loadConfig(commandLine.getOptionStrict("input-config-path"), new EqasimConfigGroup(), new DiscreteModeChoiceConfigGroup());
+
+        DiscreteModeChoiceConfigGroup discreteModeChoiceConfigGroup = (DiscreteModeChoiceConfigGroup) config.getModules().get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
+        discreteModeChoiceConfigGroup.setSelector(SelectorModule.MAXIMUM);
+
+        EqasimConfigGroup eqasimConfigGroup = (EqasimConfigGroup) config.getModules().get(EqasimConfigGroup.GROUP_NAME);
+
+
+        for(Map.Entry<String, String> entry: eqasimConfigGroup.getEstimators().entrySet()) {
+            eqasimConfigGroup.setEstimator(entry.getKey(), EpsilonModule.EPSILON_UTILITY_PREFIX + entry.getValue());
+        }
+
+        ConfigUtils.writeConfig(config, commandLine.getOptionStrict("output-config-path"));
+    }
+}
