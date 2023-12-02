55a3b37b6b73600a7de11cfda89774952d86813c
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2023-12-02 10:59:02 +0100

    feat: Pre-cleaning existing drt routes

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/IDFConfigurator.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/IDFConfigurator.java
@@ -3,7 +3,9 @@ package org.eqasim.ile_de_france;
 import org.eqasim.core.simulation.EqasimConfigurator;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.vehicles.Vehicle;
@@ -11,6 +13,8 @@ import org.matsim.vehicles.VehicleUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class IDFConfigurator extends EqasimConfigurator {
     public void adjustScenario(Scenario scenario) {
@@ -23,6 +27,16 @@ public class IDFConfigurator extends EqasimConfigurator {
                 modeVehicle.put("car", vehicleId);
                 VehicleUtils.insertVehicleIdsIntoAttributes(person, modeVehicle);
             }
+        }
+        if (config.getModules().containsKey(MultiModeDrtConfigGroup.GROUP_NAME)) {
+            MultiModeDrtConfigGroup multiModeDrtConfigGroup = (MultiModeDrtConfigGroup) config.getModules().get(MultiModeDrtConfigGroup.GROUP_NAME);
+            Set<String> drtModes = multiModeDrtConfigGroup.modes().collect(Collectors.toSet());
+            scenario.getPopulation().getPersons().values().stream()
+                    .flatMap(p -> p.getSelectedPlan().getPlanElements().stream())
+                    .filter(planElement -> planElement instanceof Leg)
+                    .map(planElement -> (Leg) planElement)
+                    .filter(leg->drtModes.contains(leg.getMode()))
+                    .forEach(leg -> leg.setRoute(null));
         }
     }
 }
