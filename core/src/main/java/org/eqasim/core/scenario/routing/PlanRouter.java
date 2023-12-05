bf6de834aa86b6e0068010c616244e308765d896
Sebastian Hörl <sebastian.horl@irt-systemx.fr>
2023-12-05 08:23:52 +0100

    fix: Handle only-walk trips in initial routing (#142)
    * fix: Handle only-walk trips in initial routing
    
    * update to latest code base
    
    * update changelog

--- core/src/main/java/org/eqasim/core/scenario/routing/PlanRouter.java
+++ core/src/main/java/org/eqasim/core/scenario/routing/PlanRouter.java
@@ -3,7 +3,9 @@ package org.eqasim.core.scenario.routing;
 import java.util.List;
 import java.util.Set;
 
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.router.TripRouter;
@@ -13,6 +15,7 @@ import org.matsim.core.utils.misc.OptionalTime;
 import org.matsim.facilities.ActivityFacilities;
 import org.matsim.facilities.FacilitiesUtils;
 import org.matsim.facilities.Facility;
+import org.matsim.utils.objectattributes.attributable.Attributes;
 
 import com.google.inject.Inject;
 
@@ -55,9 +58,31 @@ public class PlanRouter {
 					List<? extends PlanElement> newElements = tripRouter.calcRoute(mainMode, fromFacility, toFacility,
 							departureTime.seconds(), plan.getPerson(), trip.getTripAttributes());
 
+					// Fix in case we have a transit trip that is only walk
+					newElements = fixOnlyWalk(mainMode, fromFacility, toFacility, departureTime.seconds(),
+							plan.getPerson(), newElements, trip.getTripAttributes());
+
 					TripRouter.insertTrip(plan, trip.getOriginActivity(), newElements, trip.getDestinationActivity());
 				}
 			}
 		}
+	}
+
+	private List<? extends PlanElement> fixOnlyWalk(String mainMode, Facility fromFacility, Facility toFacility,
+			double departureTime, Person person, List<? extends PlanElement> elements, Attributes attributes) {
+		// No need to fix if already walk
+		if (mainMode.equals(TransportMode.walk)) {
+			return elements;
+		}
+
+		// No need to fix if we have a non-walk leg
+		for (Leg leg : TripStructureUtils.getLegs(elements)) {
+			if (!leg.getMode().equals(TransportMode.walk)) {
+				return elements;
+			}
+		}
+
+		// We have only walk legs
+		return tripRouter.calcRoute(TransportMode.walk, fromFacility, toFacility, departureTime, person, attributes);
 	}
 }
