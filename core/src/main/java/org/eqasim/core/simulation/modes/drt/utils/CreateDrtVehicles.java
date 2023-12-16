855cafa60ea3b05f098983444c284f1bf9256a77
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2023-12-16 13:13:06 +0100

    feat: random seed parameter in CreateDrtVehicles

--- core/src/main/java/org/eqasim/core/simulation/modes/drt/utils/CreateDrtVehicles.java
+++ core/src/main/java/org/eqasim/core/simulation/modes/drt/utils/CreateDrtVehicles.java
@@ -17,20 +17,24 @@ import java.util.Random;
 public class CreateDrtVehicles {
     private final static Logger logger = Logger.getLogger(CreateDrtVehicles.class);
 
+    public final static long DEFAULT_RANDOM_SEED = 1234;
+
     public static void main(String[] args) throws CommandLine.ConfigurationException, MalformedURLException {
         CommandLine cmd = new CommandLine.Builder(args) //
                 .requireOptions("network-path", "output-vehicles-path", "vehicles-number")
                 .allowOptions("vehicles-capacity", "service-begin-time", "service-end-time", "vehicle-id-prefix")
+                .allowOptions("random-seed")
                 .build();
         int vehiclesNumber = Integer.parseInt(cmd.getOptionStrict("vehicles-number"));
         int vehiclesCapacity = cmd.hasOption("vehicles-capacity") ? Integer.parseInt(cmd.getOptionStrict("vehicles-capacity")) : 4;
         int serviceBeginTime = cmd.hasOption("service-begin-time") ? Integer.parseInt(cmd.getOptionStrict("service-begin-time")) : 0;
         int serviceEndTime = cmd.hasOption("service-end-time") ? Integer.parseInt(cmd.getOptionStrict("service-end-time")) : 24 * 3600;
+        long randomSeed = cmd.hasOption("random-seed") ? Long.parseLong(cmd.getOptionStrict("random-seed")) : DEFAULT_RANDOM_SEED;
         String vehicleIdPrefx = cmd.getOption("vehicle-id-prefix").orElse("vehicle_drt_");
         Network network = NetworkUtils.createNetwork();
         new MatsimNetworkReader(network).readFile(cmd.getOptionStrict("network-path"));
         List<Id<Link>> linksIds = new ArrayList<>(network.getLinks().keySet());
-        Random random = new Random();
+        Random random = new Random(randomSeed);
         FleetSpecification fleetSpecification = new FleetSpecificationImpl();
         for(int i=0; i<vehiclesNumber; i++) {
             Id<Link> linkId = null;
