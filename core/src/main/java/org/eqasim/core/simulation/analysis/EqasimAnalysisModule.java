59bdc22905d19f3a8d910a5c75aa0ce419feaa79
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2024-01-15 11:32:24 +0100

    feat: DrtAnalysisModule is used properly now

--- core/src/main/java/org/eqasim/core/simulation/analysis/EqasimAnalysisModule.java
+++ core/src/main/java/org/eqasim/core/simulation/analysis/EqasimAnalysisModule.java
@@ -1,57 +1,30 @@
 package org.eqasim.core.simulation.analysis;
 
-import com.google.inject.Inject;
-import com.google.inject.Provider;
 import org.eqasim.core.analysis.DefaultPersonAnalysisFilter;
 import org.eqasim.core.analysis.PersonAnalysisFilter;
 import org.eqasim.core.analysis.legs.LegListener;
 import org.eqasim.core.analysis.pt.PublicTransportLegListener;
 import org.eqasim.core.analysis.trips.TripListener;
-import org.eqasim.core.components.config.EqasimConfigGroup;
-import org.eqasim.core.simulation.modes.drt.analysis.DrtAnalysisListener;
-import org.eqasim.core.simulation.modes.drt.analysis.DrtPersonAnalysisFilter;
-import org.eqasim.core.simulation.modes.drt.analysis.utils.VehicleRegistry;
+import org.eqasim.core.simulation.modes.drt.analysis.DrtAnalysisModule;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.core.controler.AbstractModule;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.controler.listener.ControlerListener;
 import org.matsim.core.router.MainModeIdentifier;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
+
 public class EqasimAnalysisModule extends AbstractModule {
 	@Override
 	public void install() {
 		addControlerListenerBinding().to(AnalysisOutputListener.class);
 		if(getConfig().getModules().containsKey(MultiModeDrtConfigGroup.GROUP_NAME)) {
-			bind(VehicleRegistry.class).asEagerSingleton();
-			addEventHandlerBinding().to(VehicleRegistry.class);
-			//Using a static method as a provider causes it to be called even when the bind is not performed
-			//So to avoid the potential problems, we use the provider below
-			addControlerListenerBinding().toProvider(new Provider<>() {
-
-				@Inject
-				EqasimConfigGroup config;
-				@Inject
-				MultiModeDrtConfigGroup drtConfig;
-				@Inject
-				OutputDirectoryHierarchy outputDirectory;
-				@Inject
-				Network network;
-				@Inject
-				VehicleRegistry vehicleRegistry;
-
-				@Override
-				public ControlerListener get() {
-					return new DrtAnalysisListener(config, drtConfig, outputDirectory, network, vehicleRegistry);
-				}
-			});
-			// Define filter for trip analysis
-			bind(PersonAnalysisFilter.class).to(DrtPersonAnalysisFilter.class);
+			install(new DrtAnalysisModule());
 		} else {
+			// Would be better if there was a way to add the module above as an overriding module from this method.
+			// That way we could simply bind the two classes below before the if clause
 			bind(DefaultPersonAnalysisFilter.class);
 			bind(PersonAnalysisFilter.class).to(DefaultPersonAnalysisFilter.class);
 		}
