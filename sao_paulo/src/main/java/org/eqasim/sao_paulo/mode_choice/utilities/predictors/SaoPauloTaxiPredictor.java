9fab628289c4c67bd43146f9b421859afceee4ec
Mayurí Annerose <mayuri.ann@gmail.com>
2024-02-20 11:51:39 -0300

    add multi-stage taxi trip to SaoPauloTaxiPredictor (#194)
    * add multi-stage taxi trip
    
    * fix wrong parameters
    
    * update changelog

--- sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/utilities/predictors/SaoPauloTaxiPredictor.java
+++ sao_paulo/src/main/java/org/eqasim/sao_paulo/mode_choice/utilities/predictors/SaoPauloTaxiPredictor.java
@@ -6,36 +6,51 @@ import org.eqasim.core.simulation.mode_choice.cost.CostModel;
 import org.eqasim.core.simulation.mode_choice.utilities.predictors.CachedVariablePredictor;
 import org.eqasim.core.simulation.mode_choice.utilities.predictors.PredictorUtils;
 import org.eqasim.sao_paulo.mode_choice.utilities.variables.TaxiVariables;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contribs.discrete_mode_choice.model.DiscreteModeChoiceTrip;
+import org.matsim.core.router.TripStructureUtils;
 
+import com.google.common.base.Verify;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 public class SaoPauloTaxiPredictor extends CachedVariablePredictor<TaxiVariables> {
 	private final CostModel costModel;
+	
 
 	@Inject
 	public SaoPauloTaxiPredictor(@Named("taxi") CostModel costModel) {
 		this.costModel = costModel;
+		
 	}
 
 	@Override
 	public TaxiVariables predict(Person person, DiscreteModeChoiceTrip trip, List<? extends PlanElement> elements) {
-		if (elements.size() > 1) {
-			throw new IllegalStateException("We do not support multi-stage taxi trips yet.");
+		
+		double taxiTravelTime_min = 0;
+		double accessEgressTime_min = 0;
+		
+		boolean foundTaxi = false;
+		
+		for (Leg leg : TripStructureUtils.getLegs(elements)) {
+			if (leg.getMode().equals(TransportMode.taxi)) {
+				Verify.verify(!foundTaxi);
+				taxiTravelTime_min += leg.getTravelTime().seconds() / 60.0;
+			} else if (leg.getMode().equals(TransportMode.walk)) {
+				accessEgressTime_min += leg.getTravelTime().seconds() / 60.0;
+			} else {
+				throw new IllegalStateException("Unexpected mode in taxi chain: " + leg.getMode());
+			}
 		}
 
-		Leg leg = (Leg) elements.get(0);
-
-		double travelTime_min = leg.getTravelTime().seconds() / 60.0;
+		
 		double cost_MU = costModel.calculateCost_MU(person, trip, elements);
-
 		double euclideanDistance_km = PredictorUtils.calculateEuclideanDistance_km(trip);
-		double accessEgressTime_min = 0;// parameters.car.constantAccessEgressWalkTime_min;
+		
 
-		return new TaxiVariables(travelTime_min, cost_MU, euclideanDistance_km, accessEgressTime_min);
+		return new TaxiVariables(taxiTravelTime_min, cost_MU, euclideanDistance_km, accessEgressTime_min);
 	}
 }
