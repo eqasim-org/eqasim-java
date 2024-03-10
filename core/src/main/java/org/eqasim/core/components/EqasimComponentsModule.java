bfecb9215017e86970bc9c70be167016bee79057
Sebastian Hörl <hoerl.sebastian@gmail.com>
2024-03-10 16:55:30 +0100

    update after merge

--- core/src/main/java/org/eqasim/core/components/EqasimComponentsModule.java
+++ core/src/main/java/org/eqasim/core/components/EqasimComponentsModule.java
@@ -3,11 +3,12 @@ package org.eqasim.core.components;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.router.AnalysisMainModeIdentifier;
 import org.matsim.core.router.MainModeIdentifier;
+import org.matsim.core.router.RoutingModeMainModeIdentifier;
 
 public class EqasimComponentsModule extends AbstractModule {
 	@Override
 	public void install() {
-		bind(MainModeIdentifier.class).to(EqasimMainModeIdentifier.class);
-		bind(AnalysisMainModeIdentifier.class).to(EqasimMainModeIdentifier.class);
+		bind(MainModeIdentifier.class).to(RoutingModeMainModeIdentifier.class);
+		bind(AnalysisMainModeIdentifier.class).to(RoutingModeMainModeIdentifier.class);
 	}
 }
