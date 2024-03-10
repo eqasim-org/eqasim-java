6aa4d1f3e83db76c281cb7c58542c3acb3075dec
Sebastian Hörl <hoerl.sebastian@gmail.com>
2024-03-10 18:19:48 +0100

    put in line with develop

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/IDFConfigurator.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/IDFConfigurator.java
@@ -1,8 +1,5 @@
 package org.eqasim.ile_de_france;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eqasim.core.simulation.EqasimConfigurator;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
@@ -12,18 +9,20 @@ import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.vehicles.Vehicle;
 import org.matsim.vehicles.VehicleUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IDFConfigurator extends EqasimConfigurator {
-	public void adjustScenario(Scenario scenario) {
-		// if there is a vehicles file defined in config, manually assign them to their
-		// agents
-		Config config = scenario.getConfig();
-		if (config.qsim().getVehiclesSource() == QSimConfigGroup.VehiclesSource.fromVehiclesData) {
-			for (Person person : scenario.getPopulation().getPersons().values()) {
-				Id<Vehicle> vehicleId = Id.createVehicleId(person.getId());
-				Map<String, Id<Vehicle>> modeVehicle = new HashMap<>();
-				modeVehicle.put("car", vehicleId);
-				VehicleUtils.insertVehicleIdsIntoAttributes(person, modeVehicle);
-			}
-		}
-	}
+    public void adjustScenario(Scenario scenario) {
+        // if there is a vehicles file defined in config, manually assign them to their agents
+        Config config = scenario.getConfig();
+        if (config.qsim().getVehiclesSource() == QSimConfigGroup.VehiclesSource.fromVehiclesData) {
+            for (Person person : scenario.getPopulation().getPersons().values()) {
+                Id<Vehicle> vehicleId = Id.createVehicleId(person.getId());
+                Map<String, Id<Vehicle>> modeVehicle = new HashMap<>();
+                modeVehicle.put("car", vehicleId);
+                VehicleUtils.insertVehicleIdsIntoAttributes(person, modeVehicle);
+            }
+        }
+    }
 }
