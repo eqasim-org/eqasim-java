85d5cc2fcc29119ce5f4e6bd2bfc53e21d6a5f3a
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2024-09-21 16:34:58 +0200

    fix: properly assigning accessFacility

--- core/src/main/java/org/eqasim/core/simulation/modes/feeder_drt/router/FeederDrtRoutingModule.java
+++ core/src/main/java/org/eqasim/core/simulation/modes/feeder_drt/router/FeederDrtRoutingModule.java
@@ -59,7 +59,7 @@ public class FeederDrtRoutingModule implements RoutingModule {
         // Identify closest stations from the origin and destination of the trip (if they are not skipped)
         Facility accessFacility = null;
         if(!skipFacility(routingRequest.getFromFacility())) {
-            this.accessEgressStopsSelector.getAccessFacility(routingRequest, this.accessEgressStopSearch.getAccessFacilities(routingRequest));
+            accessFacility = this.accessEgressStopsSelector.getAccessFacility(routingRequest, this.accessEgressStopSearch.getAccessFacilities(routingRequest));
         }
         Facility egressFacility = null;
         if(!skipFacility(routingRequest.getToFacility())) {
