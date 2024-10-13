d188796e0acecd4ac9d1c723ac795ab8bcbec1ac
Sebastian Hörl <hoerl.sebastian@gmail.com>
2024-10-13 21:50:00 +0200

    change cost calculation

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/munich/MunichPtCostModel.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/munich/MunichPtCostModel.java
@@ -129,7 +129,7 @@ public class MunichPtCostModel implements CostModel {
 	private final static double basePrice_h = 8.0;
 
 	private final static double[] prices = new double[] { //
-			3.9, 3.9, 5.8, 7.7, 9.7, 11.6, 13.6, 15.4, 17.1, 18.8, 20.5, 22.2, 25.5 //
+			3.9, 5.8, 7.7, 9.7, 11.6, 13.6, 15.4, 17.1, 18.8, 20.5, 22.2, 25.5 //
 	};
 
 	private double getZonalPrice(int firstMinimumZone, int firstMaximumZone, int lastMinimumZone, int lastMaximumZone) {
@@ -145,10 +145,10 @@ public class MunichPtCostModel implements CostModel {
 		includesM |= lastMinimumZone == 0;
 		includesM |= lastMaximumZone == 0;
 
-		if (includesM) {
-			return prices[difference + 1];
-		} else {
-			return prices[difference];
+		if (!includesM) {
+			difference -= 1;
 		}
+
+		return prices[Math.max(0, difference)];
 	}
 }
