8a7732cf8e49e22d4dc50a0aea7670ff1d1feaa3
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2025-04-20 21:07:08 +0200

    feat: using temporary directory

--- core/src/main/java/org/eqasim/core/scenario/routing/RunPopulationRouting.java
+++ core/src/main/java/org/eqasim/core/scenario/routing/RunPopulationRouting.java
@@ -1,5 +1,8 @@
 package org.eqasim.core.scenario.routing;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,7 +25,7 @@ import org.matsim.facilities.ActivityFacility;
 import com.google.inject.Injector;
 
 public class RunPopulationRouting {
-	static public void main(String[] args) throws ConfigurationException, InterruptedException {
+	static public void main(String[] args) throws ConfigurationException, InterruptedException, IOException {
 		CommandLine cmd = new CommandLine.Builder(args) //
 				.requireOptions("config-path", "output-path") //
 				.allowOptions("threads", "batch-size", "modes", EqasimConfigurator.CONFIGURATOR) //
@@ -35,6 +38,8 @@ public class RunPopulationRouting {
 		cmd.applyConfiguration(config);
 		config.replanning().clearStrategySettings();
 		VehiclesValidator.validate(config);
+
+		config.controller().setOutputDirectory(Files.createTempDirectory("tempSimulationOutputs").toAbsolutePath().toString());
 
 		int batchSize = cmd.getOption("batch-size").map(Integer::parseInt).orElse(100);
 		int numberOfThreads = cmd.getOption("threads").map(Integer::parseInt)
