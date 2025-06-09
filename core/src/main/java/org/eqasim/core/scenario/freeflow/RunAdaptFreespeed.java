cd3e83d122bb6f45f5a6c8b31e8bfa12b0fe1f58
Sebastian Hörl <hoerl.sebastian@gmail.com>
2025-06-09 20:41:18 +0200

    update

--- core/src/main/java/org/eqasim/core/scenario/freeflow/RunAdaptFreespeed.java
+++ core/src/main/java/org/eqasim/core/scenario/freeflow/RunAdaptFreespeed.java
@@ -15,25 +15,25 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RunAdaptFreespeed {
-        static public void main(String[] args)
-                        throws ConfigurationException, JacksonException, DatabindException, IOException {
-                CommandLine cmd = new CommandLine.Builder(args) //
-                                .requireOptions("input-path", "output-path", "configuration-path")
-                                .build();
+    static public void main(String[] args)
+            throws ConfigurationException, JacksonException, DatabindException, IOException {
+        CommandLine cmd = new CommandLine.Builder(args) //
+                .requireOptions("input-path", "output-path", "configuration-path")
+                .build();
 
-                String inputPath = cmd.getOptionStrict("input-path");
-                String outputPath = cmd.getOptionStrict("output-path");
-                String settingsPath = cmd.getOptionStrict("settings-path");
+        String inputPath = cmd.getOptionStrict("input-path");
+        String outputPath = cmd.getOptionStrict("output-path");
+        String settingsPath = cmd.getOptionStrict("settings-path");
 
-                ObjectMapper objectMapper = new ObjectMapper();
-                FreeflowConfiguration configuration = objectMapper.readValue(new File(settingsPath),
-                                FreeflowConfiguration.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        FreeflowConfiguration configuration = objectMapper.readValue(new File(settingsPath),
+                FreeflowConfiguration.class);
 
-                Network network = NetworkUtils.createNetwork();
-                new MatsimNetworkReader(network).readFile(inputPath);
+        Network network = NetworkUtils.createNetwork();
+        new MatsimNetworkReader(network).readFile(inputPath);
 
-                FreeflowConfigurator.create(network).apply(network, configuration);
+        FreeflowConfigurator.create(network).apply(network, configuration);
 
-                new NetworkWriter(network).write(outputPath);
-        }
+        new NetworkWriter(network).write(outputPath);
+    }
 }
