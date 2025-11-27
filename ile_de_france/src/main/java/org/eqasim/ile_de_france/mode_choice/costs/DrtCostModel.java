bd4c6dd5672e46e4f71f4d4091a3fc6aef34a194
tkchouaki <tkchouaki@gmail.com>
2025-11-27 14:08:59 +0100

    feat(ile_de_france): DRT cost model

---
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/costs/DrtCostModel.java
@@ -0,0 +1,27 @@
+package org.eqasim.ile_de_france.mode_choice.costs;
+
+import com.google.inject.Inject;
+import org.eqasim.core.simulation.mode_choice.cost.AbstractCostModel;
+import org.eqasim.ile_de_france.mode_choice.parameters.IDFCostParameters;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.contribs.discrete_mode_choice.model.DiscreteModeChoiceTrip;
+import org.matsim.core.utils.geometry.CoordUtils;
+
+import java.util.List;
+
+public class DrtCostModel extends AbstractCostModel {
+    private final IDFCostParameters costParameters;
+    @Inject
+    protected DrtCostModel(IDFCostParameters costParameters) {
+        super("drt");
+        this.costParameters = costParameters;
+
+    }
+
+    @Override
+    public double calculateCost_MU(Person person, DiscreteModeChoiceTrip trip, List<? extends PlanElement> list) {
+        double distance = CoordUtils.calcEuclideanDistance(trip.getOriginActivity().getCoord(), trip.getDestinationActivity().getCoord()) / 1000;
+        return this.costParameters.drtCost_EUR_access + this.costParameters.drtCost_EUR_km * distance;
+    }
+}
