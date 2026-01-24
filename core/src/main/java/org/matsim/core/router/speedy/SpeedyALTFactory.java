b3831ad282a52813d950494e41efab552ea48a7c
Sebastian Hörl <hoerl.sebastian@gmail.com>
2026-01-24 13:01:52 +0100

    fix caching in speedalt

---
+++ core/src/main/java/org/matsim/core/router/speedy/SpeedyALTFactory.java
@@ -0,0 +1,57 @@
+package org.matsim.core.router.speedy;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.config.groups.GlobalConfigGroup;
+import org.matsim.core.config.groups.RoutingConfigGroup;
+import org.matsim.core.router.util.LeastCostPathCalculator;
+import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author mrieser / Simunto, sponsored by SBB Swiss Federal Railways
+ * @author sebhoerl, IRT SystemX
+ */
+@Singleton
+public class SpeedyALTFactory implements LeastCostPathCalculatorFactory {
+	private final int threads;
+	private final int landmarksCount;
+
+	private final Map<Network, SpeedyGraph> graphs = new ConcurrentHashMap<>();
+	private final Map<SpeedyGraph, SpeedyALTData> landmarksData = new ConcurrentHashMap<>();
+
+	static private final int EQASIM_LANDMARKS = 32;
+
+	@Inject
+	public SpeedyALTFactory(final GlobalConfigGroup globalConfigGroup, RoutingConfigGroup routingConfig) {
+		this(globalConfigGroup.getNumberOfThreads(), EQASIM_LANDMARKS);
+	}
+
+	public SpeedyALTFactory(int threads, int landmarks) {
+		this.threads = Math.max(1, threads);
+		this.landmarksCount = landmarks;
+	}
+
+	public SpeedyALTFactory() {
+		this(4, EQASIM_LANDMARKS);
+	}
+
+	@Override
+	public LeastCostPathCalculator createPathCalculator(Network network, TravelDisutility travelCosts, TravelTime travelTimes) {
+		SpeedyGraph graph = graphs.computeIfAbsent(network, SpeedyGraphBuilder::build);
+		
+		SpeedyALTData landmarks = landmarksData.computeIfAbsent(graph, g -> {
+			int reducedLandmarksCount = Math.min(landmarksCount, g.nodeCount);
+			return new SpeedyALTData(g, reducedLandmarksCount, travelCosts, threads);
+		});
+		
+		return new SpeedyALT(landmarks, travelTimes, travelCosts);
+	}
+
+}
