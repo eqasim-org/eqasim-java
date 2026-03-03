7ed7e01035789aa84f02f7235dc7495cf11776b9
Valentin LE BESCOND <lebescond@lasa.fr>
2026-03-03 15:07:33 +0100

    update emission values

--- core/src/test/java/org/eqasim/TestEmissions.java
+++ core/src/test/java/org/eqasim/TestEmissions.java
@@ -150,11 +150,11 @@ public class TestEmissions {
 
 	private void runMelunEmissions() throws CommandLine.ConfigurationException, IOException {
 		Map<String, Long> counts = countLegs("melun_test/output/output_events.xml.gz");
-		Assert.assertEquals(4644, (long) counts.get("car"));
+		Assert.assertEquals(4641, (long) counts.get("car"));
 		Assert.assertEquals(1554, (long) counts.get("car_passenger"));
-		Assert.assertEquals(11915, (long) counts.get("walk"));
-		Assert.assertEquals(633, (long) counts.getOrDefault("bike", 0L));
-		Assert.assertEquals(2252, (long) counts.get("pt"));
+		Assert.assertEquals(11910, (long) counts.get("walk"));
+		Assert.assertEquals(638, (long) counts.getOrDefault("bike", 0L));
+		Assert.assertEquals(2251, (long) counts.get("pt"));
 
 		SafeOsmHbefaMapping.defaultType = "URB/Local/50";
 
@@ -165,7 +165,7 @@ public class TestEmissions {
 				"sample_41_EFA_HOT_SubSegm_2020detailed.csv",
 				"--eqasim-configurator", TestConfigurator.class.getName() });
 
-		assertEquals(634633, countLines(new File("melun_test/output/output_emissions_events.xml.gz")));
+		assertEquals(634526, countLines(new File("melun_test/output/output_emissions_events.xml.gz")));
 
 		RunExportEmissionsNetwork.main(new String[] { "--config-path", "melun_test/input/config.xml",
 				"--pollutants", "PM,CO,NOx,Unknown", "--time-bin-size", "3600",
@@ -173,11 +173,11 @@ public class TestEmissions {
 
 		Collection<SimpleFeature> features = ShapeFileReader.getAllFeatures("melun_test/output/emissions_network.shp");
 
-		// NOTE: Locally, I always get 42230 (old : 32527) lines here. On Github CI, it is always
+		// NOTE: Locally, I always get 42235 (old : 32527) lines here. On Github CI, it is always
 		// 32528. No clue why this is, but all the previous tests on the events line
 		// length etc. pass without a problem ...
 
-		assertEquals(42230, features.size());
+		assertEquals(42235, features.size());
 
 		SimpleFeature feature = features.stream().filter(f -> f.getAttribute("link").toString().equals("163994")
 				& f.getAttribute("time").toString().equals("43200")).findFirst().orElse(null);
