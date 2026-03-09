412d3e6f0d83d0cdf602b24ac17754bd5cd6381f
Grace <kaghog@ethz.ch>
2026-03-09 13:27:56 +0100

    update: define seville cost model and parameters

--- sutlab/src/main/java/org/sutlab/seville/mode_choice/parameters/SevilleCostParameters.java
+++ sutlab/src/main/java/org/sutlab/seville/mode_choice/parameters/SevilleCostParameters.java
@@ -4,13 +4,18 @@ import org.eqasim.core.simulation.mode_choice.ParameterDefinition;
 
 public class SevilleCostParameters implements ParameterDefinition {
 	public double carCost_EUR_km = 0.0;
-	public double parisParkingCost_EUR_h = 0.0;
+	public double busCost_EUR = 0.0;
+	public double metroCost_EUR = 0.0;
+	public double ptCost_EUR = 0.0;
 
 	public static SevilleCostParameters buildDefault() {
 		SevilleCostParameters parameters = new SevilleCostParameters();
 
+		//http://visit-seville.com/seville-metro/ https://visit-seville.com/transport/
 		parameters.carCost_EUR_km = 0.2;
-		parameters.parisParkingCost_EUR_h = 3.0;
+		parameters.busCost_EUR = 1.4;
+		parameters.metroCost_EUR = 1.35;
+		parameters.ptCost_EUR = 1.8; //Renfe tickets
 
 		return parameters;
 	}
