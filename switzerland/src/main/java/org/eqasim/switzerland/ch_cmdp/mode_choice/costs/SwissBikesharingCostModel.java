0410e164bbbd1bfec2afdd1ebb661c7ce8c7bc93
Milos Balac <balacm@ethz.ch>
2026-07-15 17:54:13 +0200

    Update SwissBikesharingCostModel.java
    implement proper cost model

--- switzerland/src/main/java/org/eqasim/switzerland/ch_cmdp/mode_choice/costs/SwissBikesharingCostModel.java
+++ switzerland/src/main/java/org/eqasim/switzerland/ch_cmdp/mode_choice/costs/SwissBikesharingCostModel.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.eqasim.core.simulation.mode_choice.cost.AbstractCostModel;
 import org.eqasim.switzerland.ch_cmdp.mode_choice.parameters.SwissBikesharingCostParameters;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contribs.discrete_mode_choice.model.DiscreteModeChoiceTrip;
@@ -21,8 +23,22 @@ public class SwissBikesharingCostModel extends AbstractCostModel {
 
     @Override
     public double calculateCost_MU(Person person, DiscreteModeChoiceTrip trip, List<? extends PlanElement> elements) {
-        double cost = 0.0;
-        return cost;
+		double travelTime_min = 0.0;
+		double distance_km = 0.0;
+
+		for (PlanElement element : elements) {
+			if (element instanceof Leg) {
+				Leg leg = (Leg) element;
+
+				if (leg.getMode().equals(TransportMode.bike)) {
+					travelTime_min += leg.getTravelTime().seconds() / 60.0;
+					distance_km += leg.getRoute().getDistance() * 1e-3;
+				}
+			}
+		}
+
+		double cost = parameters.CHF_base + parameters.CHF_min * travelTime_min + parameters.CHF_km * distance_km;
+		return Math.max(parameters.CHF_minimum, cost);
     }
 
 }
